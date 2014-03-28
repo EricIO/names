f81f9bbe16452329d9b82e8e611d0ddbbd6b96ed
Eric Skoglund <eric@pagefault.se>
2014-03-28 22:20:53 +0100

    Added test-data to verify the program.

--- name.py
+++ name.py
@@ -1,38 +1,44 @@
 from os import listdir
+
+import pickle
 import re
 
-
-dir = "names"
-fname_pattern = re.compile("yob[0-9]{4}.txt")
-alpha = 'abcdefghijklmnopqrstuvwxyz'
-
-names  = {'f':set(),'m':set()}
-ngrams = {'f':{letter: 0 for letter in alpha}, 'm':{letter: 0 for letter in alpha}}
-total_letters = 0
-
-def read_files():
-    files = fname_pattern.findall(' '.join(listdir(dir)))
+def read_data():
+    try:
+        return pickle.load(open('data.dat', 'rb'))
+    except IOError:
+        print('Unable to load file, reading data from text files')
+        
+    files = fname_pattern.findall(' '.join(listdir('names')))
+    names  = {'f':set(),'m':set()}
+    fname_pattern = re.compile("yob[0-9]{4}.txt")
     for f in files:
-        for line in open(dir + '/' + f, 'r').readlines():
+        for line in open('names' + '/' + f, 'r').readlines():
             s = line.split(',')
             names.get(s[1].lower()).add(s[0].lower()) # s[1] is the sex s[0] is the name
 
-def populate_corpus():
-    tl = 0
-    if len(names.get('f')) == 0:
-        read_files()
+    corpus = populate_corpus(names)
+    pickle.dump(corpus, open('data.dat', 'wb'))
+    return corpus
+            
+def populate_corpus(names):
+    grams = {'f':{},'m':{}}
     for sex in names:
         for name in names.get(sex):
-            tl += len(name)
-            for letter in name:
-                ngrams[sex][letter] += 1
-                
-if __name__ == "__main__":
-    read_files()
-    populate_corpus()
-    total_letters = 0
-    for l in ngrams['f']:
-        total_letters += ngrams.get('f').get(l)
-    for letters in ngrams.get('f'):
-        print(letters + " : " + str(ngrams['f'][letters])+ " : " + str(ngrams['f'][letters] / total_letters))
+            gram = bigram('<' + name + '>')
+            for l in gram:
+                if grams[sex].get(l) == None:
+                    grams[sex][l] = 1
+                else:
+                    grams[sex][l] += 1
+    return grams
 
+def bigram(name):
+    '''Returns all the bigrams for a string'''
+    return [name[i:i+2] for i in range(1+len(name)-2)]
+
+    
+if __name__ == "__main__":
+    corpus = read_data()
+    print(corpus)
+    
