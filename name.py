162def1d9457ee1563a2e3eb7f772c9761c921fd
Eric Skoglund <eric@pagefault.se>
2014-03-29 18:53:17 +0100

    The program now produces 77 procent correct on 1000 names. Will try to weight the data based on the last letter having a hunch that it will be the factor that plays the most importance

--- name.py
+++ name.py
@@ -1,7 +1,11 @@
+from functools import reduce
 from os import listdir
 
+import math
 import pickle
 import re
+import operator
+import sys
 
 def read_data():
     try:
@@ -37,8 +41,56 @@ def bigram(name):
     '''Returns all the bigrams for a string'''
     return [name[i:i+2] for i in range(1+len(name)-2)]
 
+def probability(name, data):
+    '''Computes the probabilty for the gender of the name''' 
+    female_total, male_total  = total(data)
+    grams = bigram('<' + name.lower() + '>')
+    female_probs = [(i + 1) / (female_total + len(data['f'])) for i in counts(data, grams, 'f')]
+    male_probs = [((i + 1)/ (male_total + len(data['m']))) for i in counts(data, grams, 'm')]
+    
+    return reduce(operator.add, [math.log(i) for i in female_probs]), reduce(operator.add, [math.log(i) for i in male_probs])
+
+def counts(data, grams, gender):
+    return [data[gender].get(i, 0) for i in grams]
+
+def total(data):
+    ftot, mtot = 0, 0
+    for _, v in data['f'].items():
+        ftot += v
+    for _, v in data['m'].items():
+        mtot += v
+    return ftot, mtot
+
+def gender(name,data):
+    pf,pm = probability(name,data)
+    if pf > pm:
+        return 'F'
+    else:
+        return 'M'
+        
+    
     
 if __name__ == "__main__":
     corpus = read_data()
-    print(corpus)
-    
+    if len(sys.argv) > 1:
+        g = gender('<' + sys.argv[1] + '>', corpus)
+        if g == 'F':
+            print('My guess is that you are a female!')
+        else:
+            print('My guess is that you are a male!')
+    else:
+        tot, hits, misses, unknown = 0,0,0,0
+        f = open('test-data', 'r')
+        for line in f.readlines():
+            data = line.strip('\n').split(',')
+            g = gender(data[0], corpus)
+            print("You guessed {0} was {1} and was {2}".format(data[0],g, data[1]))
+            tot += 1
+            if g == data[1]:
+                hits += 1
+            else:
+                misses += 1
+            print('Total: {0}\nHits: {1}\nMisses: {2}\nUnknow: {3}\nCorrectness: {4}'
+                  .format(tot, hits, misses, unknown, (hits/tot * 100)))
+            
+            
